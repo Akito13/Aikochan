db2140317d98a062f99f64f51584f9a3dccbe7c8
Akito13 <79491658+Akito13@users.noreply.github.com>
2021-10-28 11:05:00 +0700

    Update messageDelete.js

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -17,18 +17,21 @@ module.exports = {
             content,
             createdTimestamp
         } = message;
-        const channel = message.guild.channels.cache.find(ch => ch.name == "logs");
-        const messageDeleteEmbed = new MessageEmbed()
-            .setAuthor(`${author.tag}`, author.displayAvatarURL())
-            .setColor("RED")
-            .setDescription(`**Message deleted in ${message.channel} by ${author}**\n${content}`)
-            .setFooter(`User ID: ${author.id}`)
-            .setTimestamp(createdTimestamp);
-        const messageHadAttachment = attachments.first();
-        if (messageHadAttachment) {
-            messageDeleteEmbed.setImage(messageHadAttachment.proxyURL);
-        }
-        if (!author.bot) {
+        if (message.author.bot) {
+            return;
+        } else {
+            const channel = message.guild.channels.cache.find(ch => ch.name == "logs");
+            const messageDeleteEmbed = new MessageEmbed()
+                .setAuthor(`${author.tag}`, author.displayAvatarURL())
+                .setColor("RED")
+                .setDescription(`**Message deleted in ${message.channel} by ${author}**\n${content}`)
+                .setFooter(`User ID: ${author.id}`)
+                .setTimestamp(createdTimestamp);
+            const messageHadAttachment = attachments.first();
+            if (messageHadAttachment) {
+                messageDeleteEmbed.setImage(messageHadAttachment.proxyURL);
+            }
+
             channel.send({
                 embeds: [messageDeleteEmbed]
             })
